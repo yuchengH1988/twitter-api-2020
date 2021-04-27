b7661dab21b8ca9045f657ea235acfa3946addc9
yuchengH1988 <yucheng.h.1988@gmail.com>
2021-04-27 10:11:37 +0800

    moved socket in a file

--- app.js
+++ app.js
@@ -1,7 +1,6 @@
 const express = require('express')
 const bodyParser = require('body-parser')
 const cors = require('cors')
-const helpers = require('./_helpers')
 // .env
 if (process.env.NODE_ENV !== 'production') {
   require('dotenv').config()
@@ -9,9 +8,9 @@ if (process.env.NODE_ENV !== 'production') {
 const app = express()
 const port = process.env.PORT || 3000
 const httpServer = require('http').createServer(app)
-const options = {}
+// const options = {}
 //socket需要掛在伺服器上
-const io = require('socket.io')(httpServer, options)
+
 // 載入 cors
 app.use(cors())
 
@@ -21,43 +20,15 @@ app.use(bodyParser.json())
 
 // 載入 routes
 require('./routes')(app)
-app.get('/socketchat', (req, res) => {
-  res.sendFile(__dirname + '/index.html');
-});
 
-const username = "userxxx"
-
-io.on('connection', (socket) => {
-  //伺服器端顯示
-  console.log("user connected to socket:" + socket.id)
-  //傳送至client歡迎訊息
-  socket.emit("chat message", helpers.formatMessage(username, "Welcome to the chatroom."))
-  //傳給聊天室其他人，有人進來聊天室了
-  socket.broadcast.emit('chat message', helpers.formatMessage(username, " has joined this room."))
-  //socket打開接收器的狀態 接收data =msg
-  socket.on('chat message', msg => {
-    io.emit('chat message', helpers.formatMessage(username, msg))
-  })
-  // 離線顯示訊息
-  socket.on('disconnect', () => {
-    console.log('user :' + socket.id + ' is disconnected')
-    io.emit('chat message', helpers.formatMessage(username, " has left this room."))
-  })
+const io = require('socket.io')(httpServer)
+const socketController = require('./sockets/socketController')
+io.on('connection', function (socket) {
+  socketController.respond(socket)
 })
-// io.emit('some event', { someProperty: 'some value', otherProperty: 'other value' })
-
-// io.on('connection', (socket) => {
-//   socket.broadcast.emit('hi');
-// });
-
-// io.on('connection', (socket) => {
-//   socket.on('chat message', (msg) => {
-//     io.emit('chat message', msg);
-//   });
-// });
-
-// app.listen(port, () => console.log(`Example app listening on port ${port}!`))
-
+app.get('/socketchat', (req, res) => {
+  res.sendFile(__dirname + '/sockets/index.html');
+});
 
 httpServer.listen(port)
 
